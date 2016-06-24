0ba7bf0b276c260c77c33657428b92d6b65f2971
Zdravko Donev <kfzzdravko@gmail.com>
2016-06-24 23:13:59 +0300

    Remove unused line from local test

--- CreationalPatterns/AbstractFactoryPattern/test/factory/FactoryProducerTest.java
+++ CreationalPatterns/AbstractFactoryPattern/test/factory/FactoryProducerTest.java
@@ -10,7 +10,6 @@ public class FactoryProducerTest {
 	public void testColorFactory() {
 		AbstractFactory colorFactory = FactoryProducer.getFactory(FactoryProducer.FACTORY_COLOR);
 		assertNotNull(colorFactory);
-		assertNull(colorFactory);
 	}
 
 	@Test
